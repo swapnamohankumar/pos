c5cebf09b910db78880cc16a2409b1a74dff9c40
swapn <swapn@DESKTOP-E6IFV13>
2024-10-22 12:38:03 +0530

    Changes to Valid Login test script

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Utility;
 import page.HomePage;
 import page.LoginPage;
 
@@ -12,14 +13,18 @@ public class ValidLogin extends BaseTest
 	@Test(priority = 1)
 	public void testValidLogin()
 	{
+//		Get Test Data 
+		String un=Utility.getXLCellData(XL_PATH, "ValidLogin",1, 0);
+		String pw=Utility.getXLCellData(XL_PATH, "ValidLogin",1, 1);
+		
 //		1. enter valid un
-		test.info("enter valid User Name");
+		test.info("enter valid User Name:"+un);
 		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
 		
 //		2. enter valid pw
-		test.info("enter valid Password");
-		loginPage.setPassword("pointofsale");
+		test.info("enter valid Password:"+pw);
+		loginPage.setPassword(pw);
 		
 //		3. click on login
 		test.info("Click on Go Button");
